b5a3ea7b155b5c80c8d6fe3c045c4f181ee2fb92
sekhar-indian <munisekharudvalapti1@gmail.com>
2023-08-22 18:56:31 +0530

    day1

--- day6.js
+++ day6.js
@@ -1,2 +1,28 @@
-console.log("welcome to sekhar")
-console.log(" god")
+/*let h=document.querySelector("#main-header");
+h.style.borderBottom='solid 4px red'
+
+let d=document.querySelector("input");
+d.value="sekhar"
+
+let sub=document.querySelector('input[type="submit"]');
+sub.value="nono";
+
+let item=document.querySelector(".list-group-item");
+item.style.color="red";
+
+let litem=document.querySelector(".list-group-item:last-child")
+litem.style.color="red"  
+
+let pitem=document.querySelector(".list-group-item:nth-child(2)");
+pitem.style.color="red";   */
+
+let a=document.querySelectorAll('.title');
+console.log(a);
+a[0].textContent="helo";
+
+let odd=document.querySelectorAll('li:nth-child(odd)') 
+let evn=document.querySelectorAll('li:nth-child(even)')
+for(let i=0;i<odd.length;i++)
+{
+    odd[i].style.color="red"
+}
